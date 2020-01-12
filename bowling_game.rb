87d18976c9bf54a5b363d5326779c4fe42531217
kontaa <gitkontaa@gmail.com>
2020-01-12 12:33:30 +0900

    green

---
+++ bowling_game.rb
@@ -0,0 +1,8 @@
+class BowlingGame
+  def record_shot(pints)
+  end
+  def score
+    0
+  end
+end
+
---
+++ bowling_game_test.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+
+# Windows では winpty を使うことで、red green 表示できる
+#
+# 例
+# @sh  $ winpty ruby <test program.rb>
+# @vim :!winpty ruby %
+#
+
+require './bowling_game'
+
+class BowlingGameTest < Test::Unit::TestCase
+  test '全ての投球がガター' do
+    game = BowlingGame.new
+
+    20.times do
+      game.record_shot(0)
+    end
+
+    assert_equal 0, game.score
+  end
+end
+
