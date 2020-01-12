7173b1fc3f02305b09dda24a3ee25dbd6d2df085
kontaa <gitkontaa@gmail.com>
2020-01-12 12:35:30 +0900

    red

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -19,5 +19,16 @@ class BowlingGameTest < Test::Unit::TestCase
 
     assert_equal 0, game.score
   end
+
+  test '全ての投球で1ピンのみ' do
+    game = BowlingGame.new
+
+    20.times do
+      game.record_shot(1)
+    end
+
+    assert_equal 20, game.score
+
+  end
 end
 
