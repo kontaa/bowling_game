0aa07f903479d3bd66895ba09df1ddb2d13fbd4b
kontaa <gitkontaa@gmail.com>
2020-01-12 12:46:43 +0900

    refactoring

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -15,19 +15,21 @@ class BowlingGameTest < Test::Unit::TestCase
   end
 
   test '全ての投球がガター' do
-    20.times do
-      @game.record_shot(0)
-    end
-
+    record_shot_times(0, 20)
     assert_equal 0, @game.score
   end
 
   test '全ての投球で1ピンのみ' do
-    20.times do
-      @game.record_shot(1)
-    end
-
+    record_shot_times(1, 20)
     assert_equal 20, @game.score
+  end
+
+  private
+
+  def record_shot_times(pins, n)
+    n.times do 
+      @game.record_shot(pins)
+    end
   end
 end
 
