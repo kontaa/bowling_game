a1249caec9df7eb19480907dc1309eda46675922
kontaa <gitkontaa@gmail.com>
2020-01-12 12:43:41 +0900

    refactoring

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -10,25 +10,24 @@ require 'test/unit'
 require './bowling_game'
 
 class BowlingGameTest < Test::Unit::TestCase
-  test '全ての投球がガター' do
-    game = BowlingGame.new
+  setup do
+    @game = BowlingGame.new
+  end
 
+  test '全ての投球がガター' do
     20.times do
-      game.record_shot(0)
+      @game.record_shot(0)
     end
 
-    assert_equal 0, game.score
+    assert_equal 0, @game.score
   end
 
   test '全ての投球で1ピンのみ' do
-    game = BowlingGame.new
-
     20.times do
-      game.record_shot(1)
+      @game.record_shot(1)
     end
 
-    assert_equal 20, game.score
-
+    assert_equal 20, @game.score
   end
 end
 
