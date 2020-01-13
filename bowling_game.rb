082fa8eac86ebb21bfdf1af3a9dae73fe56bdf5b
kontaa <gitkontaa@gmail.com>
2020-01-13 11:04:03 +0900

    change spare bonus algorithm to strike's one

--- bowling_game.rb
+++ bowling_game.rb
@@ -3,7 +3,7 @@ require './frame'
 class BowlingGame
   def initialize
     @frames = [Frame.new]
-    @spare_frame = nil
+    @spare = []
     @strikes = []
   end
 
@@ -19,9 +19,9 @@ class BowlingGame
     curr_frame.record_shot(pins)
 
     spare_bonus(pins)
-    set_spare if curr_frame.spare?
-
     strike_bonus(pins)
+
+    set_spare if curr_frame.spare?
     set_strike if curr_frame.strike?
 
     @frames << Frame.new if curr_frame.finished?
@@ -40,14 +40,16 @@ class BowlingGame
   end
 
   def spare_bonus(pins)
-    if @spare_frame&.need_bonus?
-      @spare_frame.add_bonus(pins)
-      @spare_frame = nil
+    @spare = @spare.select do |f|
+      if f.need_bonus?
+        f.add_bonus(pins)
+        f.need_bonus?
+      end
     end
   end
 
   def set_spare
-    @spare_frame = curr_frame
+    @spare << curr_frame
   end
 
   def strike_bonus(pins)
